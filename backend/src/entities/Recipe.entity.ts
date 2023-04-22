5d91126ce3727a82746f02a63f15560fb6db17ca
Jan Vasiljevic <vasiljevic.jan@gmail.com>
2023-04-22 13:10:05 +0200

    Removed some migrations and added enums for tag foods

--- backend/src/entities/Recipe.entity.ts
+++ backend/src/entities/Recipe.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, Enum, Property } from '@mikro-orm/core';
 import { CustomBaseEntity } from './CustomBaseEntity';
 
 @Entity()
@@ -8,4 +8,15 @@ export class Recipe extends CustomBaseEntity {
 
   @Property()
   description: string;
+
+  @Enum({ items: () => FoodTag, array: true, default: [] })
+  foodTags: FoodTag[] = [];
+}
+
+export enum FoodTag {
+  HIGH_PROTEIN = 'High protein',
+  MEAT = 'Meat',
+  FISH = 'Fish',
+  LOW_GLYCEMIC_INDEX = 'Low glycemic index',
+  RICH_IN_FIBER = 'Rich in fiber',
 }
