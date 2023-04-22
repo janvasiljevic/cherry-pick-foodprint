d176e6ebe451b89d5f9f81c90ee47c05ea9032c2
Jan Vasiljevic <vasiljevic.jan@gmail.com>
2023-04-23 01:34:42 +0200

    ingridient api

--- backend/src/ingridient/ingridient.service.ts
+++ backend/src/ingridient/ingridient.service.ts
@@ -1,26 +1,65 @@
 import { Injectable } from '@nestjs/common';
 import { CreateIngridientDto } from './dto/create-ingridient.dto';
 import { UpdateIngridientDto } from './dto/update-ingridient.dto';
-
+import { EntityRepository, wrap } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { Ingridient } from 'src/entities/Ingridient.entity';
+import { Source } from 'src/entities/Source.entity';
 @Injectable()
 export class IngridientService {
-  create(createIngridientDto: CreateIngridientDto) {
-    return 'This action adds a new ingridient';
+  private readonly ingridientsRepository: EntityRepository<Ingridient>;
+  private readonly sourceRepository: EntityRepository<Source>;
+
+  constructor(
+    @InjectRepository(Ingridient)
+    ingridientsRepository: EntityRepository<Ingridient>,
+    @InjectRepository(Ingridient)
+    sourceRepository: EntityRepository<Source>,
+  ) {
+    this.ingridientsRepository = ingridientsRepository;
+    this.sourceRepository = sourceRepository;
   }
 
-  findAll() {
-    return `This action returns all ingridient`;
+  async create(createIngridientDto: CreateIngridientDto) {
+    const ingridient = new Ingridient();
+
+    ingridient.name = createIngridientDto.name;
+    ingridient.weight = createIngridientDto.weight;
+
+    if (createIngridientDto.sourceId) {
+      const source = await this.sourceRepository.findOne({
+        id: createIngridientDto.sourceId,
+      });
+
+      if (!source) throw new Error('Source ingridient not found');
+
+      ingridient.source = source;
+    }
+
+    await this.ingridientsRepository.persist(ingridient).flush();
+
+    return ingridient;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} ingridient`;
+  async update(id: string, updateIngridientDto: UpdateIngridientDto) {
+    const ingridient = await this.ingridientsRepository.findOne({ id });
+
+    if (!ingridient) throw new Error('Ingridient not found');
+
+    wrap(ingridient).assign(updateIngridientDto);
+
+    await this.ingridientsRepository.flush();
+
+    return ingridient;
   }
 
-  update(id: number, updateIngridientDto: UpdateIngridientDto) {
-    return `This action updates a #${id} ingridient`;
-  }
+  async remove(id: string) {
+    const ingridient = await this.ingridientsRepository.findOne({ id });
 
-  remove(id: number) {
-    return `This action removes a #${id} ingridient`;
+    if (!ingridient) throw new Error('Ingridient not found');
+
+    this.ingridientsRepository.remove(ingridient).flush();
+
+    return ingridient;
   }
 }
